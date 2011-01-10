68f6dd29fe0834bb59fb9766282bf131080b7872
mkm <kragh@diku.dk>
2011-01-10 20:37:56 +0100

    Changed indentation in test case.

--- test/test.cr
+++ test/test.cr
@@ -3,10 +3,10 @@ f2() = if 0 then 2 else 3;
 f3() = if 1 then 2 else 3;
 f4() = let x = 8 in x;
 f5(x) =
-        if eq(x, 0)
-           then 0
-           else if eq(x, 1)
-                then 1
-                else add(f5(sub(x, 1)), f5(sub(x, 2)));
+    if eq(x, 0)
+    then 0
+    else if eq(x, 1)
+         then 1
+         else add(f5(sub(x, 1)), f5(sub(x, 2)));
 f6(a, b) = sub(add(a, b), 1);
 f7(t) = add(get(t, 0), get(t, 1));
